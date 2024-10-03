df11db3ee2ab66df71013f2ee50843d3c9ba1855
FahimReza <FahimReza202@outlook.com>
2024-10-03 17:16:17 +0330

    Create The Todo App Page

--- core/TodoList/views.py
+++ core/TodoList/views.py
@@ -1,3 +1,20 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render,HttpResponse
 from django.views.generic import ListView
+from .models import Todo
 # Create your views here.
+
+
+
+class Task(ListView):
+    template_name='list_todo.html'
+    context_object_name = 'todo'
+    paginate_by = 10
+    ordering = ['id']
+
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Todo.objects.filter(user = user)
+        return queryset
