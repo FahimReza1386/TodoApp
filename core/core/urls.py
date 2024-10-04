2dc32d824e58f8334dc78a6a3d0a85a1d2199181
FahimReza <FahimReza202@outlook.com>
2024-10-04 09:43:45 +0330

    Save The Change in Html

--- core/core/urls.py
+++ core/core/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Todo/', include('TodoList.urls')),
+    path('', include('TodoList.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
