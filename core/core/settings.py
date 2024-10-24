d3c4971e47d48b493facca9107f957b43b08fa68
FahimReza <FahimReza202@outlook.com>
2024-10-24 13:17:11 +0330

    Create Api Documentation With Swagger And Redoc

--- core/core/settings.py
+++ core/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'TodoList',
     'rest_framework',
     'django_filters',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- core/core/urls.py
+++ core/core/urls.py
@@ -19,6 +19,22 @@ from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.documentation import include_docs_urls
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view 
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Api Documentation",
+      default_version='v1',
+      description="Documentation in Todo App",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="Fahim@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +42,10 @@ urlpatterns = [
     path('api-auth/',include('rest_framework.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api-docs/', include_docs_urls(title = 'Documentation Todo App')),
+    path('swagger/MyApi.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+  
 ]
 
 
